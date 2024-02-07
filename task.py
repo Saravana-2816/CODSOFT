7bbcf1e8228f94c73cffbf70478bc2d8c37e6dba
Saravana-2816 <127609968+Saravana-2816@users.noreply.github.com>
2024-02-07 11:32:18 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,10 +1,22 @@
 import random 
-import string 
-password=""
-char=string.ascii_letters
-char+=string.digits
-char+=string.punctuation
-length=int(input("Enter the number of characters:"))
-for i in range(length):
-    password+=random.choice(char)
-print(password)
+options=["Rock","Paper","Scissor"]
+player=None
+computer=random.choice(options)
+while player not in options:
+    player=input("Enter your Choice(Rock,Paper,Scissor):")
+print("PLAYER:",player)   
+print("COMPUTER:",computer)
+if(player==computer):
+    print("IT'S A TIE GAME!!")
+elif(player=="Rock" and computer=="Paper"):
+    print("YOU LOSE,BAD LUCK!!!")
+elif(player=="Rock" and computer=="Scissor"):
+    print("Congratulations, YOU WIN!!!")
+elif(player=="Paper" and computer=="Rock"):
+    print("Congratulations, YOU WIN!!!")
+elif(player=="Paper" and computer=="Scissor"):
+    print("YOU LOSE,BAD LUCK!!!")
+elif(player=="Scissor" and computer=="Rock"):
+    print("YOU LOSE,BAD LUCK!!!")  
+else:
+    print("Congratulations, YOU WIN!!!")
